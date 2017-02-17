d06668880123e0c52214ac9ae687795fe79bebb4
Linas Naginionis <linas.naginionis@storebrand.no>
2017-02-17 09:01:29 +0200

    InternalEvent can now additionally be constructed with commandId

--- src/main/java/net/soundvibe/reacto/internal/InternalEvent.java
+++ src/main/java/net/soundvibe/reacto/internal/InternalEvent.java
@@ -12,6 +12,9 @@ import java.util.*;
 
 public final class InternalEvent {
 
+    public static final String COMMAND_ID = "cmdId";
+    public static final String ERROR_EVENT_NAME = "error";
+
     public final String name;
     public final Optional<MetaData> metaData;
     public final Optional<byte[]> payload;
@@ -27,23 +30,53 @@ public final class InternalEvent {
     }
 
     private InternalEvent(Throwable error) {
-        this.name = "error";
+        this.name = ERROR_EVENT_NAME;
         this.eventType = EventType.ERROR;
         this.metaData = Optional.empty();
         this.error = Optional.ofNullable(error);
         this.payload = this.error.flatMap(Mappers::exceptionToBytes);
     }
 
+    private InternalEvent(Throwable error, String cmdId) {
+        this.name = ERROR_EVENT_NAME;
+        this.eventType = EventType.ERROR;
+        this.metaData = Optional.of(ofCmd(cmdId));
+        this.error = Optional.ofNullable(error);
+        this.payload = this.error.flatMap(Mappers::exceptionToBytes);
+    }
+
+    public Optional<String> commandId() {
+        return metaData.flatMap(meta -> meta.valueOf(COMMAND_ID));
+    }
+
+    private static MetaData ofCmd(String cmdId) {
+        return MetaData.of(COMMAND_ID, cmdId);
+    }
+
     public static InternalEvent onNext(Event event) {
         return new InternalEvent(event.name, event.metaData, event.payload, Optional.empty(), EventType.NEXT);
+    }
+
+    public static InternalEvent onNext(Event event, String cmdId) {
+        return new InternalEvent(event.name, Optional.of(ofCmd(cmdId).concat(event.metaData.orElse(MetaData.empty()))) ,
+                event.payload, Optional.empty(), EventType.NEXT);
     }
 
     public static InternalEvent onError(Throwable throwable) {
         return new InternalEvent(throwable);
     }
 
+
+    public static InternalEvent onError(Throwable throwable, String cmdId) {
+        return new InternalEvent(throwable, cmdId);
+    }
+
     public static InternalEvent onCompleted() {
         return new InternalEvent("completed", Optional.empty(), Optional.empty(), Optional.empty(), EventType.COMPLETED);
+    }
+
+    public static InternalEvent onCompleted(String cmdId) {
+        return new InternalEvent("completed", Optional.of(ofCmd(cmdId)), Optional.empty(), Optional.empty(), EventType.COMPLETED);
     }
 
     @Override
