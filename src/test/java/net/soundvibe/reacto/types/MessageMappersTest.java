184c8a4fd33b8f283b297cd7581272a7506d6a62
λinas <lnaginionis@gmail.com>
2017-08-16 23:13:25 +0300

    Updated to rxJava 2.1.3
    Improved test coverage

--- src/test/java/net/soundvibe/reacto/types/MessageMappersTest.java
+++ src/test/java/net/soundvibe/reacto/types/MessageMappersTest.java
@@ -1,12 +1,15 @@
 package net.soundvibe.reacto.types;
 
+import com.google.protobuf.ByteString;
 import net.soundvibe.reacto.errors.ReactiveException;
 import net.soundvibe.reacto.internal.*;
 import net.soundvibe.reacto.internal.proto.Messages;
+import net.soundvibe.reacto.mappers.Mappers;
 import org.junit.*;
 
 import java.util.Optional;
 
+import static net.soundvibe.reacto.internal.InternalEvent.COMMAND_ID;
 import static org.junit.Assert.assertEquals;
 
 /**
@@ -20,16 +23,18 @@ public class MessageMappersTest {
         final Messages.Command expected = Messages.Command.newBuilder()
                 .setId(id)
                 .setName("doSomething")
+                .addMetadata(Messages.Metadata.newBuilder()
+                        .setKey("key1").setValue("value1")
+                        .build())
                 .build();
 
         final Command actual = MessageMappers.toCommand(expected);
         Assert.assertEquals(id, actual.id.toString());
         assertEquals("doSomething", actual.name);
         assertEquals(Optional.empty(), actual.payload);
-        assertEquals(Optional.empty(), actual.metaData);
+        assertEquals(Optional.of(MetaData.of("key1", "value1")), actual.metaData);
     }
 
-    @SuppressWarnings("ThrowableResultOfMethodCallIgnored")
     @Test
     public void shouldMapToEventContainingException() throws Exception {
         final Messages.Event expected = Messages.Event.newBuilder()
@@ -47,5 +52,44 @@ public class MessageMappersTest {
         assertEquals("foo", actual.name);
         assertEquals(ReactiveException.class, actual.error.orElseGet(NullPointerException::new).getClass());
         assertEquals(Optional.empty(), actual.metaData);
+    }
+
+    @Test
+    public void shouldMapExceptionFromPayload() throws Exception {
+        final Messages.Event expected = Messages.Event.newBuilder()
+                .setEventType(Messages.EventType.ERROR)
+                .setId("1")
+                .setName("foo")
+                .setPayload(ByteString.copyFrom(Mappers.exceptionToBytes(new IllegalArgumentException("Error")).orElse(new byte[0])))
+                .build();
+
+        final InternalEvent actual = MessageMappers.toInternalEvent(expected);
+        assertEquals("foo", actual.name);
+        assertEquals(IllegalArgumentException.class, actual.error.orElseGet(NullPointerException::new).getClass());
+    }
+
+    @Test
+    public void shouldMapToProtoBufEvent() throws Exception {
+        InternalEvent internalEvent = InternalEvent.onError(new IllegalStateException("Error"), "cmdId");
+
+        Messages.Event actual = MessageMappers.toProtoBufEvent(internalEvent);
+        assertEquals(Messages.EventType.ERROR, actual.getEventType());
+        assertEquals(IllegalStateException.class.getName(), actual.getError().getClassName());
+        assertEquals(Messages.Metadata.newBuilder()
+                .setKey(COMMAND_ID).setValue("cmdId")
+                .build(), actual.getMetadataList().get(0));
+    }
+
+    @Test
+    public void shouldMapToProtoBufCommand() throws Exception {
+        Command command = Command.create("foo", MetaData.of("key1", "value1"));
+
+        Messages.Command actual = MessageMappers.toProtoBufCommand(command);
+
+        assertEquals(command.name, actual.getName());
+        assertEquals(command.id.toString(), actual.getId());
+        assertEquals(Messages.Metadata.newBuilder()
+                .setKey("key1").setValue("value1")
+                .build(), actual.getMetadataList().get(0));
     }
 }
